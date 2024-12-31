64890dd801febe700cb8dd47c7744232eb17a187
Min-woos <wcn67888@gmail.com>
2024-12-31 11:29:44 +0900

    메인 파일 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/homepage/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,17 +7,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
+  get lightTheme => null;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
- 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: '기차 예매',
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+      home: HomePage(),
     );
   }
 }
