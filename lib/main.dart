0ba2f77877b5c279b80b979a9c3bf8d9fef15a79
Min-woos <wcn67888@gmail.com>
2025-01-02 17:33:55 +0900

    tnwjd

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/pags/homepage/homepage.dart';
 import 'package:movie_app/presentation/pages/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'presentation/pages/home/home_page_view_model.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
